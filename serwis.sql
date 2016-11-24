ccec7be25c3b2b7e58646aa1feacb89399b17846
hubson123 <hubert.firek@o2.pl>
2016-11-24 08:56:50 +0100

    Add files via upload

--- serwis.sql
+++ serwis.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.1.14
+-- version 4.5.1
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: 23 Lis 2016, 20:16
--- Server version: 5.6.17
--- PHP Version: 5.5.12
+-- Czas generowania: 24 Lis 2016, 08:56
+-- Wersja serwera: 10.1.19-MariaDB
+-- Wersja PHP: 7.0.13
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,10 +14,10 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `serwis`
+-- Baza danych: `serwis`
 --
 
 -- --------------------------------------------------------
@@ -26,21 +26,41 @@ SET time_zone = "+00:00";
 -- Struktura tabeli dla tabeli `uzytkownicy`
 --
 
-CREATE TABLE IF NOT EXISTS `uzytkownicy` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `uzytkownicy` (
+  `id` int(11) NOT NULL,
   `login` text NOT NULL,
-  `haslo` text NOT NULL,
-  `email` text NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+  `haslo` varchar(20) NOT NULL,
+  `email` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Zrzut danych tabeli `uzytkownicy`
 --
 
 INSERT INTO `uzytkownicy` (`id`, `login`, `haslo`, `email`) VALUES
-(1, 'stach', '12345678', 'takiemail@wp.pl');
+(1, 'stach', '12345678', 'takiemail@wp.pl'),
+(2, 'marek', 'marek123', 'marekto@wp.pl'),
+(3, 'krzychu', 'krzychu123', 'krzychu@o2.pl');
 
+--
+-- Indeksy dla zrzutów tabel
+--
+
+--
+-- Indexes for table `uzytkownicy`
+--
+ALTER TABLE `uzytkownicy`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT dla tabeli `uzytkownicy`
+--
+ALTER TABLE `uzytkownicy`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
