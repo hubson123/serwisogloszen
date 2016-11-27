bd01e5d02a911be86254257d2d401ad00a12be3c
hubson123 <hubert.firek@o2.pl>
2016-11-27 18:04:40 +0100

    Add files via upload

--- serwis.sql
+++ serwis.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.1
+-- version 4.1.14
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Czas generowania: 24 Lis 2016, 08:56
--- Wersja serwera: 10.1.19-MariaDB
--- Wersja PHP: 7.0.13
+-- Generation Time: 27 Lis 2016, 18:05
+-- Server version: 5.6.17
+-- PHP Version: 5.5.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,11 +14,39 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
 
 --
--- Baza danych: `serwis`
+-- Database: `serwis`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `kategorie`
+--
+
+CREATE TABLE IF NOT EXISTS `kategorie` (
+  `idK` int(11) NOT NULL,
+  `nazwa` varchar(35) NOT NULL,
+  PRIMARY KEY (`idK`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `ogloszenie`
+--
+
+CREATE TABLE IF NOT EXISTS `ogloszenie` (
+  `idO` int(11) NOT NULL,
+  `tytul` varchar(100) NOT NULL,
+  `tresc` text NOT NULL,
+  `data_dodania` date NOT NULL,
+  `idK` int(11) DEFAULT NULL,
+  PRIMARY KEY (`idO`),
+  KEY `idK_idO` (`idK`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -26,12 +54,13 @@ SET time_zone = "+00:00";
 -- Struktura tabeli dla tabeli `uzytkownicy`
 --
 
-CREATE TABLE `uzytkownicy` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `uzytkownicy` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `login` text NOT NULL,
   `haslo` varchar(20) NOT NULL,
-  `email` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `email` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
 
 --
 -- Zrzut danych tabeli `uzytkownicy`
@@ -40,27 +69,9 @@ CREATE TABLE `uzytkownicy` (
 INSERT INTO `uzytkownicy` (`id`, `login`, `haslo`, `email`) VALUES
 (1, 'stach', '12345678', 'takiemail@wp.pl'),
 (2, 'marek', 'marek123', 'marekto@wp.pl'),
-(3, 'krzychu', 'krzychu123', 'krzychu@o2.pl');
+(3, 'krzychu', 'krzychu123', 'krzychu@o2.pl'),
+(4, 'marian', 'marian123', 'marianmendai@onet.pl');
 
---
--- Indeksy dla zrzutów tabel
---
-
---
--- Indexes for table `uzytkownicy`
---
-ALTER TABLE `uzytkownicy`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT dla tabeli `uzytkownicy`
---
-ALTER TABLE `uzytkownicy`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
